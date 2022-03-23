0034db2cf8b5c615e2a63e196b610be90be1c7b1
JertixOne <84728305+JertixOne@users.noreply.github.com>
2022-03-23 11:49:48 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,6 +1,9 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <math.h>
+#include <iostream>
 
 int64_t power(int64_t x, uint16_t n) {
-    return 0;
+   int64 y = pow(x, n);
+    return y;
 }
